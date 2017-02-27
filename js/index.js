bc934345c7dabb1811c5d0d9b17348ef3dc8274c
Signeus <kev.ms93@gmail.com>
2017-02-27 11:37:58 +0100

    Updated javascript

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 $("#login-button").click(function(event){
 		 event.preventDefault();
 	 
-	 $('form').fadeOut(500);
-	 $('.wrapper').addClass('form-success');
+	 $('.form-container').fadeOut(500);
+	 $('.wrapper').addClass('signin-success');
      $('.welcome_title').fadeIn(800);
 });
 
 $("#signup-button").click(function(event){
 		 event.preventDefault();
 	 
-	 $('form').fadeOut(500);
-	 $('.wrapper').addClass('form-signup-success');
+	 $('.form-container').fadeOut(500);
+	 $('.wrapper').addClass('signup-success');
      $('.informative_title').fadeOut(200);
      $('.welcome_title').fadeIn(800);
 });
